068292bb73354121723f6ca8c2d1fbcf824c4f92
Sebastiaan <sebastiaan.reggers@jcore.com>
2023-11-24 16:00:01 +0100

    Add mappers

--- src/main/java/nl/jcore/demo/service/SupermarketService.java
+++ src/main/java/nl/jcore/demo/service/SupermarketService.java
@@ -1,9 +1,9 @@
 package nl.jcore.demo.service;
 
 import nl.jcore.demo.entity.SupermarketEntity;
+import nl.jcore.demo.mapper.SupermarketMapper;
 import nl.jcore.demo.model.Supermarket;
 import nl.jcore.demo.repository.SupermarketRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,23 +11,23 @@ import java.util.Optional;
 
 @Service
 public class SupermarketService {
-    private final ModelMapper modelMapper;
+    private final SupermarketMapper supermarketMapper;
     private final SupermarketRepository supermarketRepository;
 
     @Autowired
-    private SupermarketService(ModelMapper modelMapper, SupermarketRepository supermarketRepository) {
-        this.modelMapper = modelMapper;
+    private SupermarketService(SupermarketMapper supermarketMapper, SupermarketRepository supermarketRepository) {
+        this.supermarketMapper = supermarketMapper;
         this.supermarketRepository = supermarketRepository;
     }
 
     public Supermarket getSupermarket(Long id) {
         Optional<SupermarketEntity> supermarket = supermarketRepository.findById(id);
-        return supermarket.map(s -> modelMapper.map(s, Supermarket.class)).orElseThrow();
+        return supermarket.map(supermarketMapper::toDto).orElseThrow();
     }
 
     public Supermarket persistSupermarket(Supermarket supermarket) {
-        SupermarketEntity supermarketEntity = modelMapper.map(supermarket, SupermarketEntity.class);
+        SupermarketEntity supermarketEntity = supermarketMapper.toEntity(supermarket);
         SupermarketEntity persistedSupermarketEntity = supermarketRepository.save(supermarketEntity);
-        return modelMapper.map(persistedSupermarketEntity, Supermarket.class);
+        return supermarketMapper.toDto(persistedSupermarketEntity);
     }
 }
