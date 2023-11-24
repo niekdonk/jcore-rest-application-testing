0d37d348e6a6b2dce182e84db75d791b0a75784b
Sebastiaan <sebastiaan.reggers@jcore.com>
2023-11-24 14:09:42 +0100

    Add address and implement database requests

--- src/main/java/nl/jcore/demo/model/Supermarket.java
+++ src/main/java/nl/jcore/demo/model/Supermarket.java
@@ -7,7 +7,6 @@ public class Supermarket {
 
     private Long id;
     private String name;
-    private String address;
-    private String city;
+    private Address address;
 
 }
--- src/main/java/nl/jcore/demo/service/SupermarketService.java
+++ src/main/java/nl/jcore/demo/service/SupermarketService.java
@@ -1,15 +1,33 @@
 package nl.jcore.demo.service;
 
+import nl.jcore.demo.entity.SupermarketEntity;
 import nl.jcore.demo.model.Supermarket;
 import nl.jcore.demo.repository.SupermarketRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class SupermarketService {
+    private final ModelMapper modelMapper;
+    private final SupermarketRepository supermarketRepository;
 
-    SupermarketRepository supermarketRepository;
+    @Autowired
+    private SupermarketService(ModelMapper modelMapper, SupermarketRepository supermarketRepository) {
+        this.modelMapper = modelMapper;
+        this.supermarketRepository = supermarketRepository;
+    }
 
     public Supermarket getSupermarket(Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Optional<SupermarketEntity> supermarket = supermarketRepository.findById(id);
+        return supermarket.map(s -> modelMapper.map(s, Supermarket.class)).orElseThrow();
+    }
+
+    public Supermarket persistSupermarket(Supermarket supermarket) {
+        SupermarketEntity supermarketEntity = modelMapper.map(supermarket, SupermarketEntity.class);
+        SupermarketEntity persistedSupermarketEntity = supermarketRepository.save(supermarketEntity);
+        return modelMapper.map(persistedSupermarketEntity, Supermarket.class);
     }
 }
