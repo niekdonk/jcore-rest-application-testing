e09f093cf05bf5ae7b679958067e22aeb4e6b863
Sebastiaan <sebastiaan.reggers@jcore.com>
2023-12-08 14:32:28 +0100

    Verwijder Junit 4

--- src/test/java/nl/jcore/demo/model/SupermarketJsonTest.java
+++ src/test/java/nl/jcore/demo/model/SupermarketJsonTest.java
@@ -1,6 +1,6 @@
 package nl.jcore.demo.model;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
