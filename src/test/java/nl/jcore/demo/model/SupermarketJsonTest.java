04aaf7326ff30975d8c94757952a1cde40c9bf8a
Sebastiaan <sebastiaan.reggers@jcore.com>
2023-12-08 14:25:12 +0100

    Maak start json test

---
+++ src/test/java/nl/jcore/demo/model/SupermarketJsonTest.java
@@ -0,0 +1,32 @@
+package nl.jcore.demo.model;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import java.io.IOException;
+
+@JsonTest
+public class SupermarketJsonTest {
+
+    @Autowired
+    private JacksonTester<Supermarket> jsonTester;
+
+    @Test
+    public void testSerialize() throws IOException {
+        Address address = new Address();
+        address.setCity("Wageningen");
+        address.setZipcode("6708 HZ");
+        address.setStreet("Tarthorst");
+        address.setHousenumber("1223");
+        Supermarket supermarket = new Supermarket();
+        supermarket.setId(5L);
+        supermarket.setName("Jumbo");
+        supermarket.setAddress(address);
+
+        JsonContent<Supermarket> result = jsonTester.write(supermarket);
+        System.out.println(result);
+    }
+}
