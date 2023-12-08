1242ffc2029303204865916a30572a8a4d889d29
Rocky Tempelaars <rocky.tempelaars@jcore.com>
2023-12-08 16:45:14 +0100

    blackbox test with restassured example

--- src/test/java/nl/jcore/demo/SupermarketBlackBoxTest.java
+++ src/test/java/nl/jcore/demo/SupermarketBlackBoxTest.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -27,15 +30,30 @@ public class SupermarketBlackBoxTest {
 				.getForObject("http://localhost:" + port + "/supermarket/1",
 				String.class);
 
-
 		assertEquals("supername", JsonPath.parse(supermarket).read("$.name"));
 		assertEquals(1, JsonPath.parse(supermarket).read("$.id", Long.class));
-
 	}
 
 	private TestRestTemplate getAuthenticatedRestTemplate(){
 		return restTemplate.withBasicAuth("user", "password");
 	}
+
+	@Test
+	@Sql("/testdata.sql")
+	void getSupermarketRestAssured() throws Exception {
+		given()
+				.auth().preemptive().basic("user", "password")
+		.when()
+				.get("http://localhost:" + port + "/supermarket/1")
+		.then()
+				.statusCode(200)
+				.body("id", equalTo(1))
+				.body("name", equalTo("supername"));
+
+
+
+	}
+
 
 }
 
